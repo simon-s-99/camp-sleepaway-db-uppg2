475d2398e02e342ca84005f4c8aed1a19ac389d0
simon-s-99 <142667693+simon-s-99@users.noreply.github.com>
2023-12-15 12:29:30 +0100

    initial visual studio solution
    Co-Authored-By: Sam <142818774+Rauwolscine@users.noreply.github.com>
    Co-Authored-By: Adam Kumlin <kumlinadam99@gmail.com>

---
+++ camp-sleepaway-vs-solution/camp-sleepaway/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace camp_sleepaway
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
