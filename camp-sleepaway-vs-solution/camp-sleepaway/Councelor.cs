b5cc1cd87ca8d98b34e5fc9b54aba8eb1c5d57c3
Adam Kumlin <skola.adamkumlin99@gmail.com>
2023-12-15 13:58:46 +0100

    create councelor class, start work on ui
    installed Spectre.Console and created a quick demo

---
+++ camp-sleepaway-vs-solution/camp-sleepaway/Councelor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace camp_sleepaway
+{
+    public enum WorkTitle
+    {
+        Teacher, Parent, Coach, Other
+    }
+
+    public class Councelor
+    {
+        public int CouncelorID { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string PhoneNumber { get; set; }
+        public required WorkTitle WorkTitle { get; set; }
+        public required DateTime JoinDate { get; set; }
+        public required DateTime LeaveDate { get; set; }
+
+    }
+}
---
+++ camp-sleepaway-vs-solution/camp-sleepaway/Program.cs
@@ -0,0 +1,98 @@
+﻿using Spectre.Console;
+
+namespace camp_sleepaway
+{
+    internal class Program
+    {
+        // Samuel Lööf, Simon Sörqvist, Adam Kumlin
+        static void Main()
+        {
+            bool running = true;
+
+            while (running)
+            {
+                var mainMenuChoice = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                    .Title("[red]What do you want to do[/]?")
+                    .PageSize(10)
+                    .MoreChoicesText("[grey](Move up and down to select an option)[/]")
+                    .AddChoices(new[] {
+                    "Add new individual", "Edit individual's phone number",
+                    "Search camper"
+                    }));
+
+                if (mainMenuChoice == "Add new individual")
+                {
+                    var addIndividualChoice = AnsiConsole.Prompt(
+                    new SelectionPrompt<string>()
+                        .Title("[red]What type of individual do you want to add[/]?")
+                        .PageSize(10)
+                        .MoreChoicesText("[grey](Move up and down to select an option)[/]")
+                        .AddChoices(new[] {
+                        "Camper", "Councelor", "NextOfKin"
+                        }));
+
+                    if (addIndividualChoice == "Camper")
+                    {
+                        //AddNewCamper();
+                    }
+                    else if (addIndividualChoice == "Councelor")
+                    {
+                        //AddNewCouncelor();
+                    }
+                    else if (addIndividualChoice == "NextOfKin")
+                    {
+                        //AddNewNextOfKin();
+                    }
+                }
+                else if (mainMenuChoice == "Edit individual's phone number")
+                {
+                    var editIndividualChoice = AnsiConsole.Prompt(
+                    new SelectionPrompt<string>()
+                        .Title("[red]Edit who's phone number[/]?")
+                        .PageSize(10)
+                        .MoreChoicesText("[grey](Move up and down to select an option)[/]")
+                        .AddChoices(new[] {
+                        "Camper", "Councelor", "NextOfKin"
+                        }));
+
+                    if (editIndividualChoice == "Camper")
+                    {
+                        //ChooseCamperToEdit();
+                        //EditCamper(camper);
+                    }
+                    else if (editIndividualChoice == "Councelor")
+                    {
+                        //ChooseCouncelorToEdit();
+                        //EditCouncelor(councelor);
+                    }
+                    else if (editIndividualChoice == "NextOfKin")
+                    {
+                        //ChooseNextOfKinToEdit();
+                        //EditNextOfKin(nextOfKin);
+                    }
+                }
+                else if (mainMenuChoice == "Search camper")
+                {
+                    var searchChoice = AnsiConsole.Prompt(
+                    new SelectionPrompt<string>()
+                        .Title("[red]Search camper by...[/]?")
+                        .PageSize(10)
+                        .MoreChoicesText("[grey](Move up and down to select an option)[/]")
+                        .AddChoices(new[] {
+                        "Councelor", "Cabin"
+                        }));
+
+                    if (searchChoice == "Councelor")
+                    {
+                        //SearchCamper(councelor);
+                    }
+                    else if (searchChoice == "Cabin")
+                    {
+                        //SearchCamper(cabin);
+                    }
+                }
+            }
+        }
+    }
+}
