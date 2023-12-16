5aeee684390b33a8ceab5931b92ba299bf1d741c
simon-s-99 <142667693+simon-s-99@users.noreply.github.com>
2023-12-16 17:20:11 +0100

    Create NextOfKin.cs

---
+++ camp-sleepaway-vs-solution/camp-sleepaway/NextOfKin.cs
@@ -0,0 +1,9 @@
+﻿namespace camp_sleepaway
+{
+    // Represents a NextOfKin table in Entity Frameworks
+    public class NextOfKin : Person
+    {
+        public required int RelatedToCamper { get; set; }
+        public string RelationType { get; set; }
+    }
+}
