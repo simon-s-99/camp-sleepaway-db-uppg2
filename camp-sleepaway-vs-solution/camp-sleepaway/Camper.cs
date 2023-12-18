85115a2383522e6cfc255819a85157a3a6b4448d
simon-s-99 <142667693+simon-s-99@users.noreply.github.com>
2023-12-18 13:28:14 +0100

    fixed key fields for EF Classes
    added [Key] for clarity
    
    Co-Authored-By: Adam Kumlin <kumlinadam99@gmail.com>

--- camp-sleepaway-vs-solution/camp-sleepaway/Camper.cs
+++ camp-sleepaway-vs-solution/camp-sleepaway/Camper.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace camp_sleepaway
 {
     public class Camper : Person
     {
-        public required int Id { get; set; }
+        [Key]
+        public int Id { get; set; }
         public required DateTime BirthDate { get; set; }
         public required DateTime JoinDate { get; set; }
         public DateTime? LeaveDate { get; set; }
