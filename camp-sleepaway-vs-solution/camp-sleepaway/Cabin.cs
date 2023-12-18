b20782478fb1d6147ce67de4caf8c98e74121f47
Sam <zaaamuel_96@hotmail.com>
2023-12-18 21:41:18 +0100

    Added a new randomizer for cabin names, and added a constructor for the "camper" class

--- camp-sleepaway-vs-solution/camp-sleepaway/Cabin.cs
+++ camp-sleepaway-vs-solution/camp-sleepaway/Cabin.cs
@@ -5,17 +5,22 @@ namespace camp_sleepaway
 {
     public class Cabin
     {
+        private static readonly HashSet<string> AssignedCabinNames = new HashSet<string>();
+
         [Key]
         public int Id { get; set; }
-        public required string CabinName { get; set; }
+        public string CabinName { get; set; }
 
         // Foreign key property to Counselor
         public int? CounselorId { get; set; }
         // Reference navigation to Counselor
         public Counselor? Counselor { get; set; }
 
-        // collection reference to Camper
+        // Collection reference to Camper
         public List<Camper> Campers { get; set; } = new();
+
+        // Parameterless constructor required by EF Core
+        public Cabin() { }
 
         [SetsRequiredMembers]
         public Cabin(string cabinName, Counselor counselor)
@@ -24,7 +29,7 @@ namespace camp_sleepaway
             Counselor = counselor;
         }
 
-        // Asks user for input via console, should primarily be called from main menu 
+        // Asks user for input via console, should primarily be called from the main menu 
         public Cabin InputCabinData()
         {
             Console.Clear();
@@ -46,5 +51,54 @@ namespace camp_sleepaway
             return cabin;
         }
 
+        // Method that generates a random cabin name. Could always add more names if needed.
+        public static string GenerateRandomCabinName()
+        {
+            string[] attributes =
+            {
+                "Lion", "Power", "Lone",
+                "Forest", "Lake", "Destiny",
+                "Majestic", "Eternal", "Golden",
+                "Whispering", "Enchanted", "Radiant",
+                "Sapphire", "Crimson", "Emerald",
+                "Celestial", "Tranquil", "Harmonious",
+                "Blazing", "Mystic", "Epic"
+            };
+
+            string[] nouns =
+            {
+                "Hearts", "Rangers", "Wolves",
+                "Retreat", "Bungalow", "End",
+                "Harmony", "Oasis", "Serenity",
+                "Haven", "Zenith", "Eclipse",
+                "Infinity", "Horizon", "Spectra",
+                "Cascades", "Aurora", "Pinnacle",
+                "Quasar", "Nebula", "Solstice"
+            };
+
+            Random random = new Random();
+
+            string attribute, noun;
+            do
+            {
+                int attributeIndex = random.Next(attributes.Length);
+                int nounIndex = random.Next(nouns.Length);
+
+                attribute = attributes[attributeIndex];
+                noun = nouns[nounIndex];
+
+            } while (!IsNameUnique(attribute, noun));
+
+            // If a name gets taken it gets added to the list and cannot be used again.
+            AssignedCabinNames.Add($"{attribute} {noun}");
+
+            return $"{attribute} {noun}";
+        }
+
+        private static bool IsNameUnique(string attribute, string noun)
+        {
+            return !AssignedCabinNames.Contains($"{attribute} {noun}");
+        }
     }
 }
+
--- camp-sleepaway-vs-solution/camp-sleepaway/Camper.cs
+++ camp-sleepaway-vs-solution/camp-sleepaway/Camper.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
 
 namespace camp_sleepaway
 {
@@ -15,6 +16,54 @@ namespace camp_sleepaway
 
         // Collection navigation to NextOfKin
         public List<NextOfKin> NextOfKins { get; set; } = new();
+
+        // Constructor for camper
+        public Camper () { }
+
+        [SetsRequiredMembers]
+        public Camper (string firstName, string lastName, string phoneNumber,
+        DateTime birthDate, DateTime joinDate, DateTime? leaveDate = null)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+            BirthDate = birthDate;
+            JoinDate = joinDate;
+            LeaveDate = leaveDate;          
+        }
+
+        public Camper InputCamperData()
+        {
+            Console.Clear();
+            Console.WriteLine("Add camper");
+            Console.WriteLine();
+
+            Console.Write("First name: ");
+            string firstName = Console.ReadLine();
+
+            Console.Write("Last name: ");
+            string lastName = Console.ReadLine();
+
+            Console.Write("Phone number: ");
+            string phoneNumber = Console.ReadLine();
+
+            Console.Write("Birth date: ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+
+            Console.Write("Join date: ");
+            DateTime joinDate = DateTime.Parse(Console.ReadLine());
+
+            Console.Write("Leave date (if there is no leave date, just press 'Enter' to skip: ");
+            DateTime? leaveDate = DateTime.TryParse(Console.ReadLine(),
+            out var parsedLeaveDate) ? parsedLeaveDate: null;
+
+            // Code for once we have a method to create a cabin
+            // Cabin cabin = CreateCabin():
+
+            Camper camper = new Camper(firstName, lastName, phoneNumber, birthDate, joinDate, leaveDate);
+
+            return camper;
+        }
 
         public Camper ChooseCamperToEdit()
         {
