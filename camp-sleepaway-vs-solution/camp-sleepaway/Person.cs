e7827edf66fbe64a346b6643e267e0ebb77f0ff8
simon-s-99 <142667693+simon-s-99@users.noreply.github.com>
2023-12-19 11:48:10 +0100

    Person.cs changed required fields to annotation style
    with error messages

--- camp-sleepaway-vs-solution/camp-sleepaway/Person.cs
+++ camp-sleepaway-vs-solution/camp-sleepaway/Person.cs
@@ -9,10 +9,13 @@ namespace camp_sleepaway
 {
     public abstract class Person
     {
+        [Required(ErrorMessage = "Invalid first name.")]
         public string FirstName { get; set; }
 
+        [Required(ErrorMessage = "Invalid last name.")]
         public string LastName { get; set; }
 
+        [Required(ErrorMessage = "Invalid phone number.")]
         public string PhoneNumber { get; set; }
     }
 }
