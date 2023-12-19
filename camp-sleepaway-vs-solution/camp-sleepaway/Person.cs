8aa8c6b7fee78a70442edaa30b9e4b31a97ca5ef
Sam <142818774+Rauwolscine@users.noreply.github.com>
2023-12-19 11:49:04 +0100

    undid changes in person class

--- camp-sleepaway-vs-solution/camp-sleepaway/Person.cs
+++ camp-sleepaway-vs-solution/camp-sleepaway/Person.cs
@@ -9,10 +9,10 @@ namespace camp_sleepaway
 {
     public abstract class Person
     {
-        public string FirstName { get; set; }
+        public required string FirstName { get; set; }
 
-        public string LastName { get; set; }
+        public required string LastName { get; set; }
 
-        public string PhoneNumber { get; set; }
+        public required string PhoneNumber { get; set; }
     }
 }
